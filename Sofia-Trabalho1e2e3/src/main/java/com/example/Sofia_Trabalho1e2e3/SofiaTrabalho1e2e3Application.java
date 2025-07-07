c8da1a91f430b140c4e74a8bc84ab425226d0b9b
sofia-piscapisca <vik31079@gmail.com>
2025-07-06 21:51:42 -0300

    Initialize Spring Project

---
+++ Sofia-Trabalho1e2e3/src/main/java/com/example/Sofia_Trabalho1e2e3/SofiaTrabalho1e2e3Application.java
@@ -0,0 +1,13 @@
+package com.example.Sofia_Trabalho1e2e3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofiaTrabalho1e2e3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofiaTrabalho1e2e3Application.class, args);
+	}
+
+}
---
+++ Sofia-Trabalho1e2e3/src/test/java/com/example/Sofia_Trabalho1e2e3/SofiaTrabalho1e2e3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sofia_Trabalho1e2e3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofiaTrabalho1e2e3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
